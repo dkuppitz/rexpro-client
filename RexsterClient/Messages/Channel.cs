6ff880353c8a3fe54f113b0c95a0d431142ac230
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2013-04-16 23:38:51 +0200

    documented message types and channels

--- RexsterClient/Messages/Channel.cs
+++ RexsterClient/Messages/Channel.cs
@@ -1,9 +1,25 @@
 ﻿namespace Rexster.Messages
 {
+    /// <summary>
+    /// Defines the different serialization channels supported by RexPro Servers.
+    /// </summary>
     public static class Channel
     {
+        /// <summary>
+        /// Console serialization channel.
+        /// </summary>
+        /// <remarks>Do not use. Only MsgPack is supported by RexsterClient.</remarks>
         public const int Console = 1;
+
+        /// <summary>
+        /// MsgPack serialization channel.
+        /// </summary>
         public const int MsgPack = 2;
+
+        /// <summary>
+        /// GraphSON serialization channel.
+        /// </summary>
+        /// <remarks>Do not use. Only MsgPack is supported by RexsterClient.</remarks>
         public const int Graphson = 3;
     }
 }
--- RexsterClient/Messages/MessageType.cs
+++ RexsterClient/Messages/MessageType.cs
@@ -1,13 +1,43 @@
 ﻿namespace Rexster.Messages
 {
+    /// <summary>
+    /// Defines the different message types accepted and sent by RexPro Servers.
+    /// </summary>
     public static class MessageType
     {
+        /// <summary>
+        /// Used for responses from the RexPro Server indicating a problem has occurred.
+        /// </summary>
         public const byte Error = 0;
+
+        /// <summary>
+        /// Used for requests to open or close a session with the RexPro Server.
+        /// </summary>
         public const byte SessionRequest = 1;
+
+        /// <summary>
+        /// Used for responses from the RexPro Server to a session request containing session information.
+        /// </summary>
         public const byte SessionResponse = 2;
+
+        /// <summary>
+        /// Used for requests to process a Gremlin script on the RexPro Server.
+        /// </summary>
         public const byte ScriptRequest = 3;
+        
+        /// <summary>
+        /// Used for responses to a script request (Console serialization channel).
+        /// </summary>
         public const byte ConsoleScriptResponse = 4;
+
+        /// <summary>
+        /// Used for responses to a script request (MsgPack serialization channel).
+        /// </summary>
         public const byte MsgPackScriptResponse = 5;
+
+        /// <summary>
+        /// Used for responses to a script request (GraphSON serialization channel).
+        /// </summary>
         public const byte GraphsonScriptResponse = 6;
     }
 }
