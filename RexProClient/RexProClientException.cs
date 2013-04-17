fe97fbfb886548acf87001e421e7493be5f04ab7
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2013-04-17 02:22:05 +0200

    renamed files, folders and types RexsterClient -> RexProClient

---
+++ RexProClient/RexProClientException.cs
@@ -0,0 +1,19 @@
+﻿namespace Rexster
+{
+    using System;
+
+    using Rexster.Messages;
+
+    public class RexProClientException : Exception
+    {
+        public RexProClientException(ErrorResponse msg)
+            : base(msg.ErrorMessage)
+        {
+            this.Session = msg.Session;
+            this.Request = msg.Request;
+        }
+
+        public byte[] Session { get; private set; }
+        public byte[] Request { get; private set; }
+    }
+}
---
+++ RexProClient/RexProClientSerializationException.cs
@@ -0,0 +1,26 @@
+﻿namespace Rexster
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    public class RexProClientSerializationException : Exception
+    {
+        public RexProClientSerializationException()
+        {
+        }
+
+        public RexProClientSerializationException(string message) : base(message)
+        {
+        }
+
+        public RexProClientSerializationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected RexProClientSerializationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
