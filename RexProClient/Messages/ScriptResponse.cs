4c41865ac4a712e0d70fe8e32c9f21a6ad6bc1b7
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2013-04-19 11:25:25 +0200

    moved DynamicScriptResponse in separate file

--- RexProClient/Messages/ScriptResponse.cs
+++ RexProClient/Messages/ScriptResponse.cs
@@ -1,24 +1,7 @@
 namespace Rexster.Messages
 {
     using System.Collections.Generic;
-
-    using MsgPack;
     using MsgPack.Serialization;
-
-    public class DynamicScriptResponse : ScriptResponse<object>, IUnpackable
-    {
-        public void UnpackFromMessage(Unpacker unpacker)
-        {
-            if (unpacker.Read()) this.Session = unpacker.Unpack<byte[]>();
-            if (unpacker.Read()) this.Request = unpacker.Unpack<byte[]>();
-            if (unpacker.Read()) this.Meta = unpacker.Unpack<Dictionary<string, object>>();
-            if (unpacker.Read())
-            {
-                this.Result = unpacker.UnpackDynamicObject();
-            }
-            if (unpacker.Read()) this.Bindings = unpacker.Unpack<Dictionary<string, object>>();
-        }
-    }
 
     public class ScriptResponse<T> : ScriptResponse
     {
